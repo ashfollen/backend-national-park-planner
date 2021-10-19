f4a00eb74b19be4b882f038524e7a493989a2adb
Ashley Follen <ashleyfollen@outlook.com>
2021-10-19 12:42:38 -0600

    auth start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        # payload => { beef: 'steak' }
+        JWT.encode(payload, 'my_s3cr3t')
+        # jwt string: "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
+    end
+    
+    def decoded_token(token)
+        # token => "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
+    
+        JWT.decode(token, 'my_s3cr3t')[0]
+        # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+        # [0] gives us the payload { "beef"=>"steak" }
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
-    def index 
-        users = User.all 
-        render json: users  
-    end
+    # def index 
+    #     users = User.all 
+    #     render json: users  
+    # end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+          render json: { user: UserSerializer.new(@user) }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :unprocessable_entity
+        end
+      end
+    
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :password)
+      end
 end
