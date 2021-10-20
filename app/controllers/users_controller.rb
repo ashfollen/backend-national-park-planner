bb9cdb4ecbe3b7db5635079a7c279f0c536a26a3
Ashley Follen <ashleyfollen@outlook.com>
2021-10-19 20:02:44 -0600

    login and create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
-    # def index 
-    #     users = User.all 
-    #     render json: users  
-    # end
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :unprocessable_entity
-        end
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :unprocessable_entity
       end
-    
-      private
-
-      def user_params
-        params.require(:user).permit(:username, :password)
-      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
+      get '/logged_in', to: 'application#logged_in?'
 end
